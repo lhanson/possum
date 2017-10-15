56b050d6d64ad492ec44d9cbcb648bfd7c599309
Lyle Hanson <lyle.hanson@wisc.edu>
2017-10-15 14:15:10 -0500

    Area rectangle geometry
    Added clipping, subtraction, and union operations to rectangular areas. This should help more intelligently clear the screen and render efficiently.

--- src/main/groovy/io/github/lhanson/possum/component/AreaComponent.groovy
+++ src/main/groovy/io/github/lhanson/possum/component/AreaComponent.groovy
@@ -5,7 +5,7 @@ import groovy.transform.Canonical
 /**
  * An area represented by x, y coordinates and its dimensions.
  */
-@Canonical
+@Canonical(includes = 'position, size')
 class AreaComponent implements GameComponent {
 	VectorComponent position
 	VectorComponent size
@@ -13,7 +13,6 @@ class AreaComponent implements GameComponent {
 	AreaComponent() {
 		position = new VectorComponent()
 		size = new VectorComponent()
-
 	}
 
 	AreaComponent(AreaComponent copy) {
@@ -58,6 +57,22 @@ class AreaComponent implements GameComponent {
 		size.y = height
 	}
 
+	int getRight() {
+		position.x + size.x
+	}
+
+	void setRight(int right) {
+		size.x = right - position.x
+	}
+
+	int getBottom() {
+		position.y + size.y
+	}
+
+	void setBottom(int bottom) {
+		size.y = bottom - position.y
+	}
+
 	/**
 	 * Determines whether this area overlaps the provided one
 	 * @param area the area to compare against
@@ -73,8 +88,132 @@ class AreaComponent implements GameComponent {
 		return true
 	}
 
+	/**
+	 * Computes the area of overlap between this area and the provided one
+	 * @param that the area to compare with
+	 * @return the area of commonality between this and that
+	 */
+	AreaComponent union(AreaComponent that) {
+		def x = Math.max(this.x, that.x)
+		def y = Math.max(this.y, that.y)
+		def w = Math.min(this.right, that.right) - x
+		def h = Math.min(this.bottom, that.bottom) - y
+		return new AreaComponent(x, y, w, h)
+	}
+
+	/**
+	 * Computes the areas remaining after subtractedAreas are subtracted from this one by
+	 * individually subtracting each area from the subject, and then subtracting the overlaps
+	 * resulting from these individual decompositions.
+	 *
+	 * @param subtractedAreas areas to be excluded from the resulting areas
+	 * @return a set of areas remaining within the clip area after subtractedAreas are removed
+	 */
+	Set<AreaComponent> subtractAll(List<AreaComponent> subtractedAreas) {
+		Set<AreaComponent> results = []
+		if (!subtractedAreas) {
+			results << this
+		} else {
+			def remainders = subtractedAreas.collect { subtract(it) }.flatten()
+			// Individual subtractions may result in overlaps between the remainder areas.
+			// If any remainders overlap, remove them both from the results and add their union.
+			results = resolveOverlaps(remainders)
+		}
+		return results
+	}
+
+	/**
+	 * Given a list of areas with possible overlaps, generates a corresponding
+	 * list of unique areas without overlap. O(n^2).
+	 *
+	 * NOTE: At the moment it's not clear to me whether a single pass
+	 * will always be sufficient to guarantee zero overlaps.
+	 *
+	 * @param areas the list of areas to resolve overlaps within
+	 * @return a list of unique areas corresponding to the input areas but with no overlaps
+	 */
+	List<AreaComponent> resolveOverlaps(List<AreaComponent> areas) {
+		Set<AreaComponent> overlappers = []
+		Set<AreaComponent> subdivisions = []
+		areas.eachWithIndex{ AreaComponent ac, int i ->
+			// Test against every area to the right of it in the list
+			for (int j = i + 1; j < areas.size(); j++) {
+				AreaComponent bc = areas[j]
+				if (ac.overlaps(bc)) {
+					overlappers.addAll([ac, bc])
+					subdivisions.add(ac.union(bc))
+				}
+			}
+		}
+		return (areas - overlappers) + subdivisions
+	}
+
+	/**
+	 * Decomposes the subject area into sub-areas excluding the subject area.
+	 *
+	 * @param subjectArea a subtracted area to be excluded from the resulting subset of areas
+	 * @return a set of areas remaining within the clip area after subtractedArea is removed
+	 */
+	Set<AreaComponent> subtract(AreaComponent subjectArea) {
+		if (!subjectArea || !this.overlaps(subjectArea)) {
+			return [this] as Set
+		}
+		// Clip subject to our area
+		AreaComponent sa = clip(subjectArea)
+
+		// Up to 4 remainders if it leaves a hole: left, above, right, below
+		Set<AreaComponent> remainders = []
+
+		// Left remainder
+		if (sa.x > x) {
+			remainders << new AreaComponent(x, sa.y, sa.x - x, sa.height)
+		}
+		// Right remainder
+		if (sa.right < right) {
+			remainders << new AreaComponent(sa.right, sa.y, width - sa.right, sa.height)
+		}
+		// Top remainder
+		if (sa.y > y) {
+			remainders << new AreaComponent(x, y, width, sa.y - y)
+		}
+		// Bottom remainder
+		if (sa.bottom < bottom) {
+			remainders << new AreaComponent(x, sa.bottom, width, bottom - sa.bottom)
+		}
+
+		return remainders
+	}
+
+	/**
+	 * If any part of the subject lies outside our area (the clip area),
+	 * returns a new area representing only the area of the subject within
+	 * the clip area.
+	 *
+	 * @param subject the area to clip
+	 * @return the area of the subject which coincides with the clip rectangle
+	 */
+	AreaComponent clip(AreaComponent subject) {
+		AreaComponent clipped = new AreaComponent(subject)
+		if (subject.x < x) {
+			clipped.x = x
+			clipped.width = subject.width - (x - subject.x)
+		}
+		if (subject.y < y) {
+			clipped.y = y
+			clipped.height = subject.height - (y - subject.y)
+		}
+		if (subject.right > right) {
+			clipped.right = right
+		}
+		if (subject.bottom > bottom) {
+			clipped.bottom = bottom
+		}
+		return clipped
+	}
+
 	@Override
 	String toString() {
 		"location [$x, $y], size [$width, $height]"
 	}
+
 }
--- src/test/groovy/io/github/lhanson/possum/component/AreaComponentTest.groovy
+++ src/test/groovy/io/github/lhanson/possum/component/AreaComponentTest.groovy
@@ -4,6 +4,19 @@ import spock.lang.Specification
 
 class AreaComponentTest extends Specification {
 
+	def "basic equality"() {
+		when:
+			def area1 = new AreaComponent(0, 0, 1, 1)
+			def area2 = new AreaComponent(0, 0, 1, 1)
+		then:
+			area1.x == area2.x
+			area1.y == area2.y
+			area1.width == area2.width
+			area1.height == area2.height
+			area1.equals(area2)
+			area1 == area2
+	}
+
 	def "Same area overlaps"() {
 		when:
 			def area1 = new AreaComponent(0, 0, 1, 1)
@@ -36,4 +49,228 @@ class AreaComponentTest extends Specification {
 			area1.overlaps(area2)
 	}
 
+	def "Union of same area"() {
+		given:
+			def area1 = new AreaComponent(0, 0, 4,4)
+			def area2 = new AreaComponent(0, 0, 4,4)
+		when:
+			def union = area1.union(area2)
+		then:
+			println "a: $union"
+			println "b: $area1"
+			union == area1
+			union == area2
+	}
+
+	def "Union with x overlap"() {
+		given:
+			def area1 = new AreaComponent(0, 0, 4, 1)
+			def area2 = new AreaComponent(1, 0, 4, 1)
+		when:
+			def union = area1.union(area2)
+		then:
+			union == new AreaComponent(1, 0, 3, 1)
+	}
+
+	def "Union with y overlap"() {
+		given:
+			def area1 = new AreaComponent(0, 0, 4, 4)
+			def area2 = new AreaComponent(0, 2, 4, 4)
+		when:
+			def union = area1.union(area2)
+		then:
+			union == new AreaComponent(0, 2, 4, 2)
+	}
+
+	def "Union with x and y overlap"() {
+		given:
+			def area1 = new AreaComponent(0, 0, 4, 4)
+			def area2 = new AreaComponent(1, 1, 4, 4)
+		when:
+			def union = area1.union(area2)
+		then:
+			union == new AreaComponent(1, 1, 3, 3)
+	}
+
+	def "Clip where subject is wholly inside"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def subjectArea = new AreaComponent(1, 1, 1, 1)
+		when:
+			AreaComponent clipped = clipArea.clip(subjectArea)
+		then:
+			clipped == subjectArea
+	}
+
+	def "Clip where subject overlaps above"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def subjectArea = new AreaComponent(0, -1, 2, 2)
+		when:
+			AreaComponent clipped = clipArea.clip(subjectArea)
+		then:
+			clipped == new AreaComponent(0, 0, 2, 1)
+	}
+
+	def "Clip where subject overlaps below"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def subjectArea = new AreaComponent(0, 1, 4, 4)
+		when:
+			AreaComponent clipped = clipArea.clip(subjectArea)
+		then:
+			clipped == new AreaComponent(0, 1, 4, 3)
+	}
+
+	def "Clip where subject overlaps left"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def subjectArea = new AreaComponent(-1, 0, 4, 4)
+		when:
+			AreaComponent clipped = clipArea.clip(subjectArea)
+		then:
+			clipped == new AreaComponent(0, 0, 3, 4)
+	}
+
+	def "Clip where subject overlaps right"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def subjectArea = new AreaComponent(1, 0, 4, 4)
+		when:
+			AreaComponent clipped = clipArea.clip(subjectArea)
+		then:
+			clipped == new AreaComponent(1, 0, 3, 4)
+	}
+
+	def "Single area subtraction with null parameter returns original area"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtract(null)
+		then:
+			remainders == [clipArea] as Set
+	}
+
+	def "Multiple area subtraction with null list parameter returns original area"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtractAll(null)
+		then:
+			remainders == [clipArea] as Set
+	}
+
+	def "Single subtraction outside clip area returns original area"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def subtractArea = new AreaComponent(6, 0, 1, 1)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtract(subtractArea)
+		then:
+			remainders == [clipArea] as Set
+	}
+
+	def "Multiple area subtraction outside clip area returns original area"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def subtractAreas = [
+					new AreaComponent(6, 0, 1, 1),
+					new AreaComponent(6, 2, 1, 1)
+			]
+		when:
+			Set<AreaComponent> remainders = clipArea.subtractAll(subtractAreas)
+		then:
+			remainders == [clipArea] as Set
+	}
+
+	def "Single subtraction with full overlap returns no area"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def subtractArea = new AreaComponent(-1, -1, 6, 6)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtract(subtractArea)
+		then:
+			remainders == [] as Set
+	}
+
+	def "4x4 area, remove 1x1 upper-left corner"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def sa = new AreaComponent(0, 0, 1, 1)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtract(sa)
+		then:
+			remainders == [
+					new AreaComponent(1, 0, 3, 1),
+					new AreaComponent(0, 1, 4, 3)
+			] as Set
+	}
+
+	def "4x4 area, remove 1x1 upper-right corner"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def sa = new AreaComponent(3, 0, 1, 1)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtract(sa)
+		then:
+			remainders == [
+					new AreaComponent(0, 0, 3, 1),
+					new AreaComponent(0, 1, 4, 3)
+			] as Set
+	}
+
+	def "4x4 area, remove 1x1 lower-left corner"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def sa = new AreaComponent(0,3, 1, 1)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtract(sa)
+		then:
+			remainders == [
+					new AreaComponent(0, 0, 4, 3),
+					new AreaComponent(1, 3, 3, 1)
+			] as Set
+	}
+
+	def "4x4 area, remove 1x1 hole in middle"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def sa = new AreaComponent(1,1, 1, 1)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtract(sa)
+		then:
+			remainders == [
+					new AreaComponent(0, 0, 4, 1),
+					new AreaComponent(0, 1, 1, 1),
+					new AreaComponent(2, 1, 2, 1),
+					new AreaComponent(0,2, 4, 2)
+			] as Set
+	}
+
+	def "4x4 area, remove 4x4 partially overlapping area"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def sa = new AreaComponent(2, -2, 4, 4)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtract(sa)
+		then:
+			remainders == [
+					new AreaComponent(0, 0, 2, 2),
+					new AreaComponent(0, 2, 4, 2)
+			] as Set
+	}
+
+	def "4x4 area, remove top corners"() {
+		given:
+			def clipArea = new AreaComponent(0, 0, 4, 4)
+			def topLeft = new AreaComponent(0, 0, 1, 1)
+			def topRight = new AreaComponent(3, 0, 1, 1)
+		when:
+			Set<AreaComponent> remainders = clipArea.subtractAll([topLeft, topRight])
+		then:
+			remainders == [
+					new AreaComponent(1, 0, 2, 1),
+					new AreaComponent(0, 1, 4, 3)
+			] as Set
+	}
 }
