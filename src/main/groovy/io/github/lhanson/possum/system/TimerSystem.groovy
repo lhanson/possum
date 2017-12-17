6be12544e17daea9358c57be6217a4cf714d952b
Lyle Hanson <lyle.hanson@wisc.edu>
2017-12-17 14:58:54 -0600

    Optimizing TimerSystem entity lookup
    Instead of iterating over all entities for every update to identify
    timer components, we utilize the scene's lookup facility once on
    initialization to grab a reference to any timers, and subscribe to
    component add/remove notifications to maintain that list.

--- src/main/groovy/io/github/lhanson/possum/system/TimerSystem.groovy
+++ src/main/groovy/io/github/lhanson/possum/system/TimerSystem.groovy
@@ -1,26 +1,53 @@
 package io.github.lhanson.possum.system
 
 import io.github.lhanson.possum.component.TimerComponent
+import io.github.lhanson.possum.entity.GameEntity
+import io.github.lhanson.possum.events.ComponentAddedEvent
+import io.github.lhanson.possum.events.ComponentRemovedEvent
+import io.github.lhanson.possum.events.Subscription
 import io.github.lhanson.possum.scene.Scene
 import org.springframework.stereotype.Component
 
 @Component
 class TimerSystem extends GameSystem {
 	String name = 'timerSystem'
+	List<GameEntity> timers
+
+	@Override
+	void doInitScene(Scene scene) {
+		scene.eventBroker.subscribe(this)
+		timers = scene.entities.findAll {
+			it.components.findAll { it instanceof TimerComponent }
+		}
+	}
 
 	@Override
 	void doUpdate(Scene scene, double ticks) {
-		scene.entities.each { entity ->
-			entity.components
-					.findAll { it instanceof TimerComponent }
+		timers.each { entity ->
+			entity.getComponentsOfType(TimerComponent)
 					.each { TimerComponent tc ->
-						log.trace "TimerComponent, ticks left: ${tc.ticksRemaining}"
-						tc.ticksRemaining -= ticks
-						if (tc.ticksRemaining <= 0) {
-							tc.alarm()
-							entity.components.remove(tc)
-						}
-					}
+				log.trace "TimerComponent, ticks left: ${tc.ticksRemaining}"
+				tc.ticksRemaining -= ticks
+				if (tc.ticksRemaining <= 0) {
+					tc.alarm()
+					entity.components.remove(tc)
+				}
+			}
 		}
 	}
+
+	@Subscription
+	void componentAdded(ComponentAddedEvent event) {
+		if (event.component instanceof TimerComponent) {
+			log.debug("Added {} to timer lookup list", event.entity)
+			timers.add(event.entity)
+		}
+	}
+
+	@Subscription
+	void componentRemoved(ComponentRemovedEvent event) {
+		timers.remove(event.entity)
+		log.debug("Removed {} from timer lookup list", event.entity)
+	}
+
 }
