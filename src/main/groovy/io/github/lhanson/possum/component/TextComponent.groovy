7caee9d286bcd769765087d5143216fcddcf12cb
Lyle Hanson <lyle.hanson@wisc.edu>
2018-01-18 18:59:35 -0600

    TextComponents can now have modifiers (e.g., BOLD)

--- src/main/groovy/io/github/lhanson/possum/component/TextComponent.groovy
+++ src/main/groovy/io/github/lhanson/possum/component/TextComponent.groovy
@@ -4,12 +4,27 @@ package io.github.lhanson.possum.component
  * Component used to represent displayed text
  */
 class TextComponent implements GameComponent {
+	enum Modifier {
+		BOLD
+	}
+
 	String name = 'textComponent'
 	String text = ''
+	Set<Modifier> modifiers
 
 	TextComponent() { }
 
 	TextComponent(String text) {
-		this.text = text
+		this(text, [] as Set)
 	}
+
+	TextComponent(String text, Modifier modifier) {
+		this(text, [modifier] as Set)
+	}
+
+	TextComponent(String text, Set<Modifier> modifiers) {
+		this.text = text
+		this.modifiers = modifiers
+	}
+
 }
